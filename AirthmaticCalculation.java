e5f31749d0b3f221e8731dd94a28ea0fb696b888
Common12 <40684195+Common12@users.noreply.github.com>
2018-06-29 22:06:42 +0530

    Add files via upload
    division is done

---
+++ AirthmaticCalculation.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airthmaticcalculation;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HOME
+ */
+public class AirthmaticCalculation {
+
+    public static double Addition(double x,double y){
+        return x+y;
+    }public static double Subtraction(double x,double y){
+        return x-y;
+    }
+    public static double Multiplication(double x,double y){
+        return x*y;
+    }
+     public static double Division(double x,double y){
+        return x/y;
+    }
+    
+    public static void main(String[] args) {
+         try{
+       
+        String s1=   JOptionPane.showInputDialog("Enter 1st number");
+        double num1=Integer.parseInt(s1);
+        String s2=   JOptionPane.showInputDialog("Enter 2nd number");
+        double num2=Integer.parseInt(s2);
+        
+        double add=AirthmaticCalculation.Addition(num1, num2);
+       JOptionPane.showMessageDialog(null, "Sum of "+num1+" and "+num2+" is "+add);
+       
+       double subtract=AirthmaticCalculation.Subtraction(num1, num2);
+       JOptionPane.showMessageDialog(null, "Difference of "+num1+" and "+num2+" is "+subtract);
+       
+        double multiply=AirthmaticCalculation.Multiplication(num1, num2);
+        JOptionPane.showMessageDialog( null, "Product of "+num1+" and "+num2+" is "+multiply);
+        
+        double divide=AirthmaticCalculation.Multiplication(num1, num2);
+        JOptionPane.showMessageDialog( null, "division of "+num1+" and "+num2+" is "+divide);
+        
+         }catch(Exception e){
+            JOptionPane.showMessageDialog( null, "Enter number only", "Failure", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+}
